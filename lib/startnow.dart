2dbfa6ce02bd7d1bbda851010bb506f6a0ded450
Gourav <gouravxportsoft@gmail.com>
2023-06-07 11:24:33 +0530

    .

--- lib/startnow.dart
+++ lib/startnow.dart
@@ -211,68 +211,69 @@ class _startnowState extends State<startnow> {
                 height: 30,
               ),
               SizedBox(
-                  height: 100,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.popAndPushNamed(context, 'reportscreen');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
+                height: 100,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.popAndPushNamed(context, 'reportscreen');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(colors: [
+                        Color.fromARGB(255, 253, 238, 236),
+                        Color.fromARGB(255, 253, 238, 236)
+                      ]),
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    child: Ink(
-                      decoration: BoxDecoration(
-                        gradient: const LinearGradient(colors: [
-                          Color.fromARGB(255, 253, 238, 236),
-                          Color.fromARGB(255, 253, 238, 236)
-                        ]),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Container(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Container(
-                                  alignment: Alignment.center,
-                                  child: const Text(
-                                    "  Blood test guide",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black,
-                                        fontSize: 20),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 5.0,
-                                ),
-                                Image.asset(
-                                  'assets/fonts/images/open-book_1f4d6.png',
-                                  height: 22,
-                                  width: 22,
-                                ),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Container(
-                                alignment: Alignment.centerLeft,
+                    child: Container(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                alignment: Alignment.center,
                                 child: const Text(
-                                  "  Know it all about your blood & it's makers",
+                                  "  Blood test guide",
                                   style: TextStyle(
+                                      fontWeight: FontWeight.bold,
                                       color: Colors.black,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w300),
-                                ))
-                          ],
-                        ),
+                                      fontSize: 20),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 5.0,
+                              ),
+                              Image.asset(
+                                'assets/fonts/images/open-book_1f4d6.png',
+                                height: 22,
+                                width: 22,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                              alignment: Alignment.centerLeft,
+                              child: const Text(
+                                "  Know it all about your blood & it's makers",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w300),
+                              ))
+                        ],
                       ),
                     ),
-                  )),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
